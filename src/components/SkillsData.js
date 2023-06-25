7b1e4d2e0a66984be355f433c400204c3439f221
Mit mirani <mitmirani454@gmail.com>
2023-06-25 19:33:33 +0530

    Skills page completed.

---
+++ src/components/SkillsData.js
@@ -0,0 +1,64 @@
+const skills = {
+   1:[
+      {
+      title:"HTML",
+      level:6,
+      },
+      {
+      title:"CSS",
+      level:6,
+      },
+      {
+      title:"JavaScript",
+      level:5,
+      },
+      {
+      title:"React JS",
+      level:4,
+      },
+      {
+      title:"Next JS",
+      level:3,
+      },
+      {
+      title:"Three JS",
+      level:3,
+      },
+      {
+      title:"Tailwind CSS",
+      level:4,
+      },
+      {
+      title:"Figma",
+      level:4,
+      },
+      {
+      title:"Bootstrap",
+      level:5,
+      },
+   ],
+   2:[
+      {
+      title:"Node JS",
+      level:3,
+      },
+      {
+      title:"MySQL",
+      level:4,
+      },
+      {
+      title:"MongoDB",
+      level:3,
+      },
+      {
+      title:"Git",
+      level:4,
+      },
+      {
+      title:"GitHub",
+      level:4,
+      },   
+   ],
+};
+
+export default skills;
--- src/components/SkillsMenu.js
+++ src/components/SkillsMenu.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, {Component}from 'react'
 import "../styles/skillsmenu.css"
-const SkillsMenu = () => {
-  return (
-    <div>
-      
+import classNames from 'classnames'
+import skills from "./SkillsData"
+import frontendIcon from "../assets/eagle-emblem.png"
+import backendIcon from "../assets/hawk-emblem.png"
+
+
+export default class SkillsMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeItem: 1,   
+  }
+}
+handleMenuItemClick = (menuItem) => {
+  this.setState({
+    activeItem: menuItem,
+  });
+};
+
+
+renderContent = (skills) =>{
+  return skills.map((skill,index)=>(
+    <div key={index}
+      className={`skill-sub-container-${this.state.activeItem}`}
+    >
+      <h3>{skill.title}</h3>
+      <div className='level-container'>
+      {[...Array(6)].map((_,i)=>(
+        <div key={i} className={
+          `level-point ${i<skill.level? 
+            'filled': 'unfilled'}`}
+        />
+      ))}
+      </div>
     </div>
-  )
+  ));
+}
+  render() {
+    const {activeItem} = this.state;
+    const menuItems = ["FRONTEND","BACKEND"];
+
+    const currentIcon = activeItem === 1 ? frontendIcon : backendIcon;
+  
+    return(
+      <div className='skill-menu'>
+        {menuItems.map((item,index)=>(
+          <div
+            key={index}
+            className={classNames("skill-item",{activeSkills:activeItem === index+1})}
+            onClick={()=>this.handleMenuItemClick(index + 1)}
+          >
+            <h2 className='skill-title'>{item}</h2>
+          </div>
+        ))}
+        <img className='skill-icon' src={currentIcon} alt="current skill" />
+        <div className='skill-sub-container'>
+          {this.renderContent(skills[activeItem])}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default SkillsMenu
