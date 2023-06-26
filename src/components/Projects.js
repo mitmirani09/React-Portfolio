b8b5ae3338ff32a5243a103a052ef048e0f9bbcc
Mit mirani <mitmirani454@gmail.com>
2023-06-26 22:53:38 +0530

    Portfolio Completed

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import ProjectMenu from './ProjectsMenu'
 export default class Projects extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <>
+        <ProjectMenu />
+      </>
     )
   }
 }
---
+++ src/components/ProjectsData.js
@@ -0,0 +1,52 @@
+import projectOne from '../assets/project-1.png'
+import projectTwo from '../assets/project-2.png'
+import projectThree from '../assets/project-3.png'
+
+
+const projects = {
+   1:{
+         title:'3-D Portfolio',
+         image: projectOne,
+         description:(
+            <>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium 
+                  quaerat molestias minima ab, cum deleniti!
+               </p>
+            </>
+         ),
+         github:'https://github.com/mitmirani09/3D-Portfolio',
+         demo:'https://netlify.com'
+      
+      },
+   2:{
+         title:'Social Media Platform',
+         image: projectTwo,
+         description:(
+            <>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium 
+                  quaerat molestias minima ab, cum deleniti!
+               </p>
+            </>
+         ),
+         github:'https://github.com/mitmirani09/3D-Portfolio',
+         demo:'https://netlify.com'
+      
+      },
+   3:{
+         title:'Sports E-commerce',
+         image: projectThree,
+         description:(
+            <>
+               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium 
+                  quaerat molestias minima ab, cum deleniti!
+               </p>
+            </>
+         ),
+         github:'https://github.com/mitmirani09/3D-Portfolio',
+         demo:'https://netlify.com'
+      
+      },
+   
+}
+
+export default projects;
