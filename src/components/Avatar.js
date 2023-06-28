e8489e00acded87bdbd0c6af230a85d252466a95
Mit mirani <mitmirani454@gmail.com>
2023-06-28 18:31:40 +0530

    Portfolio Completed

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import avatarImg from '../assets/avatar-image.png'
+import avatarImg from '../assets/AvatarImg.png'
 import '../styles/avatar.css'
 
 
